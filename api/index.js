9f7d5c2122cb9733f0cb095d243014c157f969cc
Peter Ajtai <peter.ajtai@soliddigital.com>
2021-05-14 15:10:24 -0700

    Auto open

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import { dirname, sep } from 'path';
 import { fileURLToPath } from 'url';
+import open from "open";
 import { router } from "./router.js";
 import { ensureTodoYamlFile } from "./file-io/todo-yaml-ensure.js";
 
@@ -16,5 +17,7 @@ app.use(express.static([__dirname,"..", "app", "dist"].join(sep)));
 
 app.use("/api", router);
 app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`)
+    const url = `http://localhost:${port}`;
+    console.log(`App listening at ${url}`);
+    open(url);
 });
